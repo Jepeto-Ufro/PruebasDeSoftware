7979fc0a05ffbc9720b711f98934fb2421847471
JepetoGepeto <95377384+JepetoGepeto@users.noreply.github.com>
2022-09-20 19:13:09 -0300

    Add files via upload

---
+++ src/Kata 8/Multiply/src/test/java/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    @Test
+    void testSomething() {
+        double a = 2;
+        double b = 4;
+        assertEquals(b, Multiply.multiply(a,a));
+    }
+}
